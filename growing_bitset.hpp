15bbbee58b19fb1e6ec9f2dfda1c4a4b9fcb105b
Jochen Topf <jochen@topf.org>
2016-01-19 17:26:27 +0100

    Cleanup growing_bitset part 1.

--- growing_bitset.hpp
+++ growing_bitset.hpp
@@ -1,77 +1,74 @@
 #ifndef GROWING_BITSET_HPP
 #define GROWING_BITSET_HPP
 
-#include <iostream>
-#include <vector>
 #include <memory>
+#include <vector>
 
 #include <osmium/osm/types.hpp>
 
-class growing_bitset
-{
-private:
-    typedef std::vector<bool> bitvec_t;
-    typedef bitvec_t* bitvec_ptr_t;
-    typedef std::vector< bitvec_ptr_t > bitmap_t;
-
+class growing_bitset {
 
     static const size_t segment_size = 50*1024*1024;
-    bitmap_t bitmap;
 
-    bitvec_ptr_t find_segment (size_t segment) {
+    typedef std::vector<bool> bitvec_type;
+    typedef bitvec_type* bitvec_ptr_type;
+
+    std::vector<bitvec_ptr_type> bitmap;
+
+    static size_t segment(const osmium::object_id_type pos) {
+        return pos / static_cast<osmium::object_id_type>(segment_size);
+    }
+
+    static size_t segmented_pos(const osmium::object_id_type pos) {
+        return pos % static_cast<osmium::object_id_type>(segment_size);
+    }
+
+    bitvec_ptr_type find_segment(size_t segment) {
         if (segment >= bitmap.size()) {
             bitmap.resize(segment+1);
         }
 
-        bitvec_ptr_t ptr = bitmap.at(segment);
-        if(!ptr) {
+        auto ptr = bitmap[segment];
+        if (!ptr) {
             bitmap[segment] = ptr = new std::vector<bool>(segment_size);
         }
 
         return ptr;
     }
 
-    bitvec_ptr_t find_segment (size_t segment) const {
+    bitvec_ptr_type find_segment(size_t segment) const {
         if (segment >= bitmap.size()) {
             return nullptr;
         }
 
-        return bitmap.at(segment);
+        return bitmap[segment];
     }
 
 public:
+
     ~growing_bitset() {
-        for (bitmap_t::iterator it=bitmap.begin(), end=bitmap.end(); it != end; it++) {
-            bitvec_ptr_t ptr = (*it);
-            if(ptr) delete ptr;
+        for (auto ptr : bitmap) {
+            if (ptr) delete ptr;
         }
     }
 
     void set(const osmium::object_id_type pos) {
-        size_t
-            segment = static_cast<osmium::object_id_type>(pos) / static_cast<osmium::object_id_type>(segment_size),
-            segmented_pos = static_cast<osmium::object_id_type>(pos) % static_cast<osmium::object_id_type>(segment_size);
-
-        bitvec_ptr_t bitvec = find_segment(segment);
-        bitvec->at(segmented_pos) = true;
+        bitvec_ptr_type bitvec = find_segment(segment(pos));
+        bitvec->at(segmented_pos(pos)) = true;
     }
 
     bool get(const osmium::object_id_type pos) const {
-        size_t
-            segment = static_cast<osmium::object_id_type>(pos) / static_cast<osmium::object_id_type>(segment_size),
-            segmented_pos = static_cast<osmium::object_id_type>(pos) % static_cast<osmium::object_id_type>(segment_size);
-
-        bitvec_ptr_t bitvec = find_segment(segment);
-        if(!bitvec) return false;
-        return static_cast<bool>(bitvec->at(segmented_pos));
+        bitvec_ptr_type bitvec = find_segment(segment(pos));
+        if (!bitvec) return false;
+        return bitvec->at(segmented_pos(pos));
     }
 
     void clear() {
-        for (bitmap_t::iterator it=bitmap.begin(), end=bitmap.end(); it != end; it++) {
-            bitvec_ptr_t ptr = (*it);
+        for (auto ptr : bitmap) {
             ptr->clear();
         }
     }
-};
+
+}; // class growing_bitset
 
 #endif
